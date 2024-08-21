4b5f5858a59f9a7501020d505eca63d8bce1a95e
Reyhaneh Rahmanipour <r.rahmanipour@gmail.com>
2024-08-21 11:10:55 +0330

    add weather app url

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('loadWeatherApp').addEventListener('click', function() {
     const weatherAppContainer = document.getElementById('weatherAppContainer');
     const iframe = document.createElement('iframe');
-    iframe.src = 'https://your-weather-app-url.com'; // Replace with your actual weather app URL
+    iframe.src = 'https://rayka-weather-app.web.app/'; // Replace with your actual weather app URL
     weatherAppContainer.innerHTML = '';
     weatherAppContainer.appendChild(iframe);
 });
