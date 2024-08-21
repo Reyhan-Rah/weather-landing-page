4d722a9e30b79cc03e9133be17e42687b42951b5
Reyhaneh Rahmanipour <r.rahmanipour@gmail.com>
2024-08-21 13:25:35 +0330

    add location permission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.getElementById('loadWeatherApp').addEventListener('click', function() {
     const weatherAppContainer = document.getElementById('weatherAppContainer');
     const iframe = document.createElement('iframe');
-    iframe.src = 'https://rayka-weather-app.web.app/'; // Replace with your actual weather app URL
+    iframe.src = 'https://rayka-weather-app.web.app/';
+    iframe.allow = 'geolocation';
     weatherAppContainer.innerHTML = '';
     weatherAppContainer.appendChild(iframe);
 });
